cd1409519e47ed8d386b44b880147a4939cc9f2e
jamin <ot.zz@qq.com>
2016-09-18 17:09:56 +0800

    skip

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -10,6 +10,8 @@ USING_NS_CC_EXT;
 #include<string>
 #include<io.h>
 #include "CustomTableViewCell.h"
+#include "VisibleRect.h"
+
 using namespace std;
 
 CCLayer *layer;
@@ -22,6 +24,7 @@ vector<string> files;
 vector < string> targetFiles;
 int filesNum;
 bool initStatu = false;
+
 
 //Billboard
 //Camera3D
@@ -72,29 +75,7 @@ bool HelloWorld::init()
     closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
 
-    // create menu, it's an autorelease object
- //   auto menu = Menu::create(closeItem, NULL);
- //   menu->setPosition(Vec2::ZERO);
- //   this->addChild(menu, 1);
 
-
-	//auto closeItem2 = MenuItemImage::create(
-	//	"CloseNormal.png",
-	//	"CloseSelected.png",
-	//	CC_CALLBACK_1(HelloWorld::initCamera, this));
-
-	//closeItem2->setPosition(Vec2(50, 50));
-
-	//// create menu, it's an autorelease object
-	//auto menu2= Menu::create(closeItem2, NULL);
-	//menu2->setPosition(Vec2::ONE);
-	//this->addChild(menu2, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
     
     auto label = Label::createWithTTF("Select a file on the right", "fonts/Marker Felt.ttf", 24);
     
@@ -134,6 +115,14 @@ bool HelloWorld::init()
 	listViewLayer->addChild(tableView);
 	tableView->reloadData();
 
+	_incRot = nullptr;
+	_decRot = nullptr;
+	_camera = nullptr;
+	_bZoomOut = false;
+	_bZoomIn = false;
+	_bRotateLeft = false;
+	_bRotateRight = false;
+
 	//auto sp3d = Sprite3D::create();
 	//sp3d->setPosition(s.width,0);
 	//addChild(sp3d);
@@ -145,27 +134,28 @@ bool HelloWorld::init()
 	//bill1->setScale(0.6f);
 	//sp3d->addChild(bill1);
 	//如果目录不为空则默认显示第一个C3B文件
-	if (!targetFiles.empty())
-	{
-		createSpriteFormPath(targetFiles[0]);
-	}
+	
+	//if (!targetFiles.empty())
+	//{
+	//	createSpriteFormPath(targetFiles[0]);
+	//}
 
     return true;
 }
 
 
 //如果你重写了onEnter方法，你应该调用它的父类
-void HelloWorld::onEnter()
-{
-	Node::onEnter();
-	initCamera();
-
-}
-void HelloWorld::onExit()
-{
-	Node::onExit();
-
-}
+//void HelloWorld::onEnter()
+//{
+//	Node::onEnter();
+//	initCamera();
+//
+//}
+//void HelloWorld::onExit()
+//{
+//	Node::onExit();
+//
+//}
 //void HelloWorld::onEnterTransitionDidFinish()
 //{
 //
@@ -412,7 +402,9 @@ void HelloWorld::tableCellTouched(TableView* table, TableViewCell* cell)
 	//CCLOG("cell touched at index: %ld", static_cast<long>(cell->getIdx()));
 	CCLOG("tableCellTouched == %s", targetFiles[cell->getIdx()].c_str());
 
-	createSpriteFormPath(targetFiles[cell->getIdx()].c_str());
+	//createSpriteFormPath(targetFiles[cell->getIdx()].c_str());
+	addNewSpriteWithCoords(Vec3(0, 0, 0), targetFiles[cell->getIdx()].c_str(), true, 1, true);
+
 	//initCamera();
 }
 
@@ -492,4 +484,553 @@ ssize_t HelloWorld::numberOfCellsInTableView(TableView *table)
 	return filesNum;
 }
 
+//------------------------------------------------------------------
+//
+// Camera3DTestDemo
+//
+//------------------------------------------------------------------
+//HelloWorld::Camera3DTestDemo(void)
+//: _incRot(nullptr)
+//, _decRot(nullptr)
+//, _camera(nullptr)
+//, _bZoomOut(false)
+//, _bZoomIn(false)
+//, _bRotateLeft(false)
+//, _bRotateRight(false)
+//{
+//}
+//HelloWorld::~Camera3DTestDemo(void)
+//{
+//}
+void HelloWorld::reachEndCallBack()
+{
+}
+//std::string HelloWorld::title() const
+//{
+//	return "Testing Camera";
+//}
+
+void HelloWorld::scaleCameraCallback(Ref* sender, float value)
+{
+	if (_camera&& _cameraType != CameraType::FirstPerson)
+	{
+		Vec3 cameraPos = _camera->getPosition3D();
+		cameraPos += cameraPos.getNormalized()*value;
+		_camera->setPosition3D(cameraPos);
+	}
+}
+void HelloWorld::rotateCameraCallback(Ref* sender, float value)
+{
+	if (_cameraType == CameraType::Free || _cameraType == CameraType::FirstPerson)
+	{
+		Vec3  rotation3D = _camera->getRotation3D();
+		rotation3D.y += value;
+		_camera->setRotation3D(rotation3D);
+	}
+}
+void HelloWorld::SwitchViewCallback(Ref* sender, CameraType cameraType)
+{
+	if (_cameraType == cameraType)
+	{
+		return;
+	}
+	_cameraType = cameraType;
+	if (_cameraType == CameraType::Free)
+	{
+		_camera->setPosition3D(Vec3(0, 130, 130) + _sprite3D->getPosition3D());
+		_camera->lookAt(_sprite3D->getPosition3D());
+
+		_RotateRightlabel->setColor(Color3B::WHITE);
+		_RotateLeftlabel->setColor(Color3B::WHITE);
+		_ZoomInlabel->setColor(Color3B::WHITE);
+		_ZoomOutlabel->setColor(Color3B::WHITE);
+	}
+	else if (_cameraType == CameraType::FirstPerson)
+	{
+		Vec3 newFaceDir;
+		_sprite3D->getWorldToNodeTransform().getForwardVector(&newFaceDir);
+		newFaceDir.normalize();
+		_camera->setPosition3D(Vec3(0, 35, 0) + _sprite3D->getPosition3D());
+		_camera->lookAt(_sprite3D->getPosition3D() + newFaceDir * 50);
+
+		_RotateRightlabel->setColor(Color3B::WHITE);
+		_RotateLeftlabel->setColor(Color3B::WHITE);
+		_ZoomInlabel->setColor(Color3B::GRAY);
+		_ZoomOutlabel->setColor(Color3B::GRAY);
+	}
+	else if (_cameraType == CameraType::ThirdPerson)
+	{
+		_camera->setPosition3D(Vec3(0, 130, 130) + _sprite3D->getPosition3D());
+		_camera->lookAt(_sprite3D->getPosition3D());
+
+		_RotateRightlabel->setColor(Color3B::GRAY);
+		_RotateLeftlabel->setColor(Color3B::GRAY);
+		_ZoomInlabel->setColor(Color3B::WHITE);
+		_ZoomOutlabel->setColor(Color3B::WHITE);
+	}
+}
+
+void HelloWorld::initSprite()
+{
+	_sprite3D = nullptr;
+	auto s = Director::getInstance()->getWinSize();
+	auto listener = EventListenerTouchAllAtOnce::create();
+	listener->onTouchesBegan = CC_CALLBACK_2(HelloWorld::onTouchesBegan, this);
+	listener->onTouchesMoved = CC_CALLBACK_2(HelloWorld::onTouchesMoved, this);
+	listener->onTouchesEnded = CC_CALLBACK_2(HelloWorld::onTouchesEnded, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+	auto layer3D = Layer::create();
+	addChild(layer3D, 0);
+	_layer3D = layer3D;
+	_curState = State_None;
+	if (targetFiles.empty())
+	{
+		return;
+	}
+
+	//转换路径
+	size_t pos;
+	while ((pos = targetFiles[1].find_first_of("/")) != std::string::npos)
+	{
+		targetFiles[1].replace(pos, 1, "\\");
+	}
+	addNewSpriteWithCoords(Vec3(0, 0, 0), targetFiles[1], true, 1, true);
+	TTFConfig ttfConfig("fonts/arial.ttf", 20);
+
+	auto containerForLabel1 = Node::create();
+	_ZoomOutlabel = Label::createWithTTF(ttfConfig, "zoom out");
+	_ZoomOutlabel->setPosition(s.width - listViewLayer->getContentSize().width, VisibleRect::top().y - 30);
+	_ZoomOutlabel->setAnchorPoint(Vec2(1, 0.5));
+	containerForLabel1->addChild(_ZoomOutlabel);
+	addChild(containerForLabel1, 10);
+
+	auto listener1 = EventListenerTouchOneByOne::create();
+	listener1->setSwallowTouches(true);
+
+	listener1->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchesZoomOut, this);
+	listener1->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchesZoomOutEnd, this);
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener1, _ZoomOutlabel);
+
+	auto containerForLabel2 = Node::create();
+	_ZoomInlabel = Label::createWithTTF(ttfConfig, "zoom in");
+	_ZoomInlabel->setPosition(s.width - listViewLayer->getContentSize().width, VisibleRect::top().y - 100);
+	_ZoomInlabel->setAnchorPoint(Vec2(1, 0.5));
+	containerForLabel2->addChild(_ZoomInlabel);
+	addChild(containerForLabel2, 10);
+
+	auto listener2 = EventListenerTouchOneByOne::create();
+	listener2->setSwallowTouches(true);
+
+	listener2->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchesZoomIn, this);
+	listener2->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchesZoomInEnd, this);
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener2, _ZoomInlabel);
+
+	auto containerForLabel3 = Node::create();
+	_RotateLeftlabel = Label::createWithTTF(ttfConfig, "rotate left");
+	_RotateLeftlabel->setPosition(s.width - listViewLayer->getContentSize().width, VisibleRect::top().y - 170);
+	containerForLabel3->addChild(_RotateLeftlabel);
+	_RotateLeftlabel->setAnchorPoint(Vec2(1, 0.5));
+
+	addChild(containerForLabel3, 10);
+
+	auto listener3 = EventListenerTouchOneByOne::create();
+	listener3->setSwallowTouches(true);
+
+	listener3->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchesRotateLeft, this);
+	listener3->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchesRotateLeftEnd, this);
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener3, _RotateLeftlabel);
+
+	auto containerForLabel4 = Node::create();
+	_RotateRightlabel = Label::createWithTTF(ttfConfig, "rotate right");
+	_RotateRightlabel->setPosition(s.width - listViewLayer->getContentSize().width, VisibleRect::top().y - 240);
+	_RotateRightlabel->setAnchorPoint(Vec2(1, 0.5));
+	containerForLabel4->addChild(_RotateRightlabel);
+	addChild(containerForLabel4, 10);
+
+	auto listener4 = EventListenerTouchOneByOne::create();
+	listener4->setSwallowTouches(true);
+
+	listener4->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchesRotateRight, this);
+	listener4->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchesRotateRightEnd, this);
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener4, _RotateRightlabel);
+
+	//几个按钮
+	auto label1 = Label::createWithTTF(ttfConfig, "free ");
+	auto menuItem1 = MenuItemLabel::create(label1, CC_CALLBACK_1(HelloWorld::SwitchViewCallback, this, CameraType::Free));
+	auto label2 = Label::createWithTTF(ttfConfig, "third person");
+	auto menuItem2 = MenuItemLabel::create(label2, CC_CALLBACK_1(HelloWorld::SwitchViewCallback, this, CameraType::ThirdPerson));
+	auto label3 = Label::createWithTTF(ttfConfig, "first person");
+	auto menuItem3 = MenuItemLabel::create(label3, CC_CALLBACK_1(HelloWorld::SwitchViewCallback, this, CameraType::FirstPerson));
+	auto menu = Menu::create(menuItem1, menuItem2, menuItem3, nullptr);
+
+	menu->setPosition(Vec2::ZERO);
+
+	menuItem1->setPosition(VisibleRect::left().x + 100, VisibleRect::top().y - 50);
+	menuItem2->setPosition(VisibleRect::left().x + 100, VisibleRect::top().y - 100);
+	menuItem3->setPosition(VisibleRect::left().x + 100, VisibleRect::top().y - 150);
+	addChild(menu, 0);
+	schedule(CC_SCHEDULE_SELECTOR(HelloWorld::updateCamera), 0.0f);
+	if (_camera == nullptr)
+	{
+		_camera = Camera::createPerspective(60, (GLfloat)s.width / s.height, 1, 1000);
+		_camera->setCameraFlag(CameraFlag::USER1);
+		_layer3D->addChild(_camera);
+	}
+	SwitchViewCallback(this, CameraType::ThirdPerson);
+	DrawNode3D* line = DrawNode3D::create();
+	//draw x
+	for (int j = -20; j <= 20; j++)
+	{
+		line->drawLine(Vec3(-100, 0, 5 * j), Vec3(100, 0, 5 * j), Color4F(1, 0, 0, 1));
+	}
+	//draw z
+	for (int j = -20; j <= 20; j++)
+	{
+		line->drawLine(Vec3(5 * j, 0, -100), Vec3(5 * j, 0, 100), Color4F(0, 0, 1, 1));
+	}
+	//draw y
+	line->drawLine(Vec3(0, -50, 0), Vec3(0, 0, 0), Color4F(0, 0.5, 0, 1));
+	line->drawLine(Vec3(0, 0, 0), Vec3(0, 50, 0), Color4F(0, 1, 0, 1));
+	_layer3D->addChild(line);
+
+	_layer3D->setCameraMask(2);
+}
+void HelloWorld::onEnter()
+{
+	Node::onEnter();
+	initSprite();
+		
+}
+void HelloWorld::onExit()
+{
+	Node::onExit();
+	if (_camera)
+	{
+		_camera = nullptr;
+	}
+}
+
+void HelloWorld::addNewSpriteWithCoords(Vec3 p, std::string fileName, bool playAnimation, float scale, bool bindCamera)
+{
+	auto sprite = Sprite3D::create(fileName);
+	_layer3D->addChild(sprite);
+	float globalZOrder = sprite->getGlobalZOrder();
+	sprite->setPosition3D(Vec3(p.x, p.y, p.z));
+	sprite->setGlobalZOrder(globalZOrder);
+	if (playAnimation)
+	{
+		auto animation = Animation3D::create(fileName, "Take 001");
+		if (animation)
+		{
+			auto animate = Animate3D::create(animation);
+			sprite->runAction(RepeatForever::create(animate));
+		}
+	}
+	if (bindCamera)
+	{
+		_sprite3D = sprite;
+	}
+	sprite->setScale(scale);
+}
+void HelloWorld::onTouchesBegan(const std::vector<Touch*>& touches, cocos2d::Event  *event)
+{
+	for (auto &item : touches)
+	{
+		auto touch = item;
+		auto location = touch->getLocation();
+	}
+}
+void HelloWorld::onTouchesMoved(const std::vector<Touch*>& touches, cocos2d::Event  *event)
+{
+	if (touches.size() == 1)
+	{
+		auto touch = touches[0];
+		auto location = touch->getLocation();
+		Point newPos = touch->getPreviousLocation() - location;
+		if (_cameraType == CameraType::Free || _cameraType == CameraType::FirstPerson)
+		{
+			Vec3 cameraDir;
+			Vec3 cameraRightDir;
+			_camera->getNodeToWorldTransform().getForwardVector(&cameraDir);
+			cameraDir.normalize();
+			cameraDir.y = 0;
+			_camera->getNodeToWorldTransform().getRightVector(&cameraRightDir);
+			cameraRightDir.normalize();
+			cameraRightDir.y = 0;
+			Vec3 cameraPos = _camera->getPosition3D();
+			cameraPos += cameraDir*newPos.y*0.1f;
+			cameraPos += cameraRightDir*newPos.x*0.1f;
+			_camera->setPosition3D(cameraPos);
+			if (_sprite3D &&  _cameraType == CameraType::FirstPerson)
+			{
+				_sprite3D->setPosition3D(Vec3(_camera->getPositionX(), 0, _camera->getPositionZ()));
+				_targetPos = _sprite3D->getPosition3D();
+			}
+		}
+	}
+}
+void HelloWorld::move3D(float elapsedTime)
+{
+	if (_sprite3D)
+	{
+		Vec3 curPos = _sprite3D->getPosition3D();
+		Vec3 newFaceDir = _targetPos - curPos;
+		newFaceDir.y = 0.0f;
+		newFaceDir.normalize();
+		Vec3 offset = newFaceDir * 25.0f * elapsedTime;
+		curPos += offset;
+		_sprite3D->setPosition3D(curPos);
+		if (_cameraType == CameraType::ThirdPerson)
+		{
+			Vec3 cameraPos = _camera->getPosition3D();
+			cameraPos.x += offset.x;
+			cameraPos.z += offset.z;
+			_camera->setPosition3D(cameraPos);
+		}
+	}
+}
+void HelloWorld::updateState(float elapsedTime)
+{
+	if (_sprite3D)
+	{
+		Vec3 curPos = _sprite3D->getPosition3D();
+		Vec3 curFaceDir;
+		_sprite3D->getNodeToWorldTransform().getForwardVector(&curFaceDir);
+		//curFaceDir = -curFaceDir;
+		curFaceDir.normalize();
+		Vec3 newFaceDir = _targetPos - curPos;
+		newFaceDir.y = 0.0f;
+		newFaceDir.normalize();
+		float cosAngle = std::fabs(Vec3::dot(curFaceDir, newFaceDir) - 1.0f);
+		float dist = curPos.distanceSquared(_targetPos);
+		if (dist <= 4.0f)
+		{
+			if (cosAngle <= 0.01f)
+				_curState = State_Idle;
+			else
+				_curState = State_Rotate;
+		}
+		else
+		{
+			if (cosAngle > 0.01f)
+				_curState = State_Rotate | State_Move;
+			else
+				_curState = State_Move;
+		}
+	}
+}
+void HelloWorld::onTouchesEnded(const std::vector<Touch*>& touches, cocos2d::Event  *event)
+{
+	for (auto &item : touches)
+	{
+		auto touch = item;
+		auto location = touch->getLocationInView();
+		if (_camera)
+		{
+			if (_sprite3D && _cameraType == CameraType::ThirdPerson && _bZoomOut == false && _bZoomIn == false && _bRotateLeft == false && _bRotateRight == false)
+			{
+				Vec3 nearP(location.x, location.y, -1.0f), farP(location.x, location.y, 1.0f);
+
+				auto size = Director::getInstance()->getWinSize();
+				nearP = _camera->unproject(nearP);
+				farP = _camera->unproject(farP);
+				Vec3 dir(farP - nearP);
+				float dist = 0.0f;
+				float ndd = Vec3::dot(Vec3(0, 1, 0), dir);
+				if (ndd == 0)
+					dist = 0.0f;
+				float ndo = Vec3::dot(Vec3(0, 1, 0), nearP);
+				dist = (0 - ndo) / ndd;
+				Vec3 p = nearP + dist *  dir;
+
+				if (p.x > 100)
+					p.x = 100;
+				if (p.x < -100)
+					p.x = -100;
+				if (p.z > 100)
+					p.z = 100;
+				if (p.z < -100)
+					p.z = -100;
+
+				_targetPos = p;
+			}
+		}
+	}
+}
+void onTouchesCancelled(const std::vector<Touch*>& touches, cocos2d::Event  *event)
+{
+}
+void HelloWorld::updateCamera(float fDelta)
+{
+	if (_sprite3D)
+	{
+		if (_cameraType == CameraType::ThirdPerson)
+		{
+			updateState(fDelta);
+			if (isState(_curState, State_Move))
+			{
+				move3D(fDelta);
+				if (isState(_curState, State_Rotate))
+				{
+					Vec3 curPos = _sprite3D->getPosition3D();
+
+					Vec3 newFaceDir = _targetPos - curPos;
+					newFaceDir.y = 0;
+					newFaceDir.normalize();
+					Vec3 up;
+					_sprite3D->getNodeToWorldTransform().getUpVector(&up);
+					up.normalize();
+					Vec3 right;
+					Vec3::cross(-newFaceDir, up, &right);
+					right.normalize();
+					Vec3 pos = Vec3(0, 0, 0);
+					Mat4 mat;
+					mat.m[0] = right.x;
+					mat.m[1] = right.y;
+					mat.m[2] = right.z;
+					mat.m[3] = 0.0f;
+
+					mat.m[4] = up.x;
+					mat.m[5] = up.y;
+					mat.m[6] = up.z;
+					mat.m[7] = 0.0f;
+
+					mat.m[8] = newFaceDir.x;
+					mat.m[9] = newFaceDir.y;
+					mat.m[10] = newFaceDir.z;
+					mat.m[11] = 0.0f;
+
+					mat.m[12] = pos.x;
+					mat.m[13] = pos.y;
+					mat.m[14] = pos.z;
+					mat.m[15] = 1.0f;
+					_sprite3D->setAdditionalTransform(&mat);
+				}
+			}
+		}
+		if (_bZoomOut == true)
+		{
+			if (_camera)
+			{
+				if (_cameraType == CameraType::ThirdPerson)
+				{
+					Vec3 lookDir = _camera->getPosition3D() - _sprite3D->getPosition3D();
+					Vec3 cameraPos = _camera->getPosition3D();
+					if (lookDir.length() <= 300)
+					{
+						cameraPos += lookDir.getNormalized();
+						_camera->setPosition3D(cameraPos);
+					}
+				}
+				else if (_cameraType == CameraType::Free)
+				{
+					Vec3 cameraPos = _camera->getPosition3D();
+					if (cameraPos.length() <= 300)
+					{
+						cameraPos += cameraPos.getNormalized();
+						_camera->setPosition3D(cameraPos);
+					}
+				}
+			}
+		}
+		if (_bZoomIn == true)
+		{
+			if (_camera)
+			{
+				if (_cameraType == CameraType::ThirdPerson)
+				{
+					Vec3 lookDir = _camera->getPosition3D() - _sprite3D->getPosition3D();
+					Vec3 cameraPos = _camera->getPosition3D();
+					if (lookDir.length() >= 50)
+					{
+						cameraPos -= lookDir.getNormalized();
+						_camera->setPosition3D(cameraPos);
+					}
+				}
+				else if (_cameraType == CameraType::Free)
+				{
+					Vec3 cameraPos = _camera->getPosition3D();
+					if (cameraPos.length() >= 50)
+					{
+						cameraPos -= cameraPos.getNormalized();
+						_camera->setPosition3D(cameraPos);
+					}
+				}
+			}
+		}
+		if (_bRotateLeft == true)
+		{
+			if (_cameraType == CameraType::Free || _cameraType == CameraType::FirstPerson)
+			{
+				Vec3  rotation3D = _camera->getRotation3D();
+				rotation3D.y += 1;
+				_camera->setRotation3D(rotation3D);
+			}
+		}
+		if (_bRotateRight == true)
+		{
+			if (_cameraType == CameraType::Free || _cameraType == CameraType::FirstPerson)
+			{
+				Vec3  rotation3D = _camera->getRotation3D();
+				rotation3D.y -= 1;
+				_camera->setRotation3D(rotation3D);
+			}
+		}
+	}
+}
+bool HelloWorld::onTouchesCommon(Touch* touch, Event* event, bool* touchProperty)
+{
+	auto target = static_cast<Label*>(event->getCurrentTarget());
+
+	Vec2 locationInNode = target->convertToNodeSpace(touch->getLocation());
+	Size s = target->getContentSize();
+	Rect rect = Rect(0, 0, s.width, s.height);
+
+	if (rect.containsPoint(locationInNode))
+	{
+		*touchProperty = true;
+		return true;
+	}
+	return false;
+}
+bool HelloWorld::isState(unsigned int state, unsigned int bit) const
+{
+	return (state & bit) == bit;
+}
+bool HelloWorld::onTouchesZoomOut(Touch* touch, Event* event)
+{
+	return HelloWorld::onTouchesCommon(touch, event, &_bZoomOut);
+}
+void HelloWorld::onTouchesZoomOutEnd(Touch* touch, Event* event)
+{
+	_bZoomOut = false;
+}
+bool HelloWorld::onTouchesZoomIn(Touch* touch, Event* event)
+{
+	return HelloWorld::onTouchesCommon(touch, event, &_bZoomIn);
+}
+void HelloWorld::onTouchesZoomInEnd(Touch* touch, Event* event)
+{
+	_bZoomIn = false;
+}
+bool HelloWorld::onTouchesRotateLeft(Touch* touch, Event* event)
+{
+	return HelloWorld::onTouchesCommon(touch, event, &_bRotateLeft);
+}
+void HelloWorld::onTouchesRotateLeftEnd(Touch* touch, Event* event)
+{
+	_bRotateLeft = false;
+}
+bool HelloWorld::onTouchesRotateRight(Touch* touch, Event* event)
+{
+	return HelloWorld::onTouchesCommon(touch, event, &_bRotateRight);
+}
+void HelloWorld::onTouchesRotateRightEnd(Touch* touch, Event* event)
+{
+	_bRotateRight = false;
+}
 
--- HelloWorldScene.h
+++ HelloWorldScene.h
@@ -10,8 +10,29 @@
 #include <string>
 #include <cstringt.h>
 #include <locale>
+#include "DrawNode3D.h"
+
 
 using namespace std;
+
+enum class CameraType
+{
+	Free = 0,
+	FirstPerson = 1,
+	ThirdPerson = 2,
+};
+enum State
+{
+	State_None = 0,
+	State_Idle = 0x01,
+	State_Move = 0x02,
+	State_Rotate = 0x04,
+	State_Speak = 0x08,
+	State_MeleeAttack = 0x10,
+	State_RemoteAttack = 0x20,
+	State_Attack = 0x40,
+};
+
 class HelloWorld : public cocos2d::Layer, public cocos2d::extension::TableViewDelegate, cocos2d::extension::TableViewDataSource
 {
 public:
@@ -47,6 +68,36 @@ public:
 
 	virtual void update(float dt) override;
 
+	void reachEndCallBack();
+	void switchViewCallback(cocos2d::Ref* sender);
+	void addSpriteCallback(cocos2d::Ref* sender);
+	void delSpriteCallback(cocos2d::Ref* sender);
+	void scaleCameraCallback(cocos2d::Ref* sender, float value);
+	void rotateCameraCallback(cocos2d::Ref* sender, float value);
+	void SwitchViewCallback(cocos2d::Ref* sender, CameraType cameraType);
+	void updateCamera(float fDelta);
+	void move3D(float elapsedTime);
+	void updateState(float elapsedTime);
+	bool isState(unsigned int state, unsigned int bit) const;
+
+	bool onTouchesCommon(cocos2d::Touch* touch, cocos2d::Event* event, bool* touchProperty);
+	bool onTouchesZoomOut(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchesZoomOutEnd(cocos2d::Touch* touch, cocos2d::Event* event);
+	bool onTouchesZoomIn(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchesZoomInEnd(cocos2d::Touch* touch, cocos2d::Event* event);
+
+	bool onTouchesRotateLeft(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchesRotateLeftEnd(cocos2d::Touch* touch, cocos2d::Event* event);
+	bool onTouchesRotateRight(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchesRotateRightEnd(cocos2d::Touch* touch, cocos2d::Event* event);
+	void addNewSpriteWithCoords(cocos2d::Vec3 p, std::string fileName, bool playAnimation = false, float scale = 1.0f, bool bindCamera = false);
+
+	void onTouchesBegan(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event  *event);
+	void onTouchesMoved(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event  *event);
+	void onTouchesEnded(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event  *event);
+
+	void initSprite();
+
 private:
 	std::vector<cocos2d::Sprite3D*> _sprits;
 	int _animateQuality;
@@ -58,6 +109,25 @@ protected:
 	cocos2d::EventListenerTouchOneByOne* _lis;
 	cocos2d::BillBoard* bill1;
 	cocos2d::Camera *_camera;
+
+	std::string    _title;
+	cocos2d::Layer*         _layer3D;
+	cocos2d::Sprite3D*      _sprite3D;
+	cocos2d::Vec3           _targetPos;
+	CameraType     _cameraType;
+	cocos2d::MenuItem*      _incRot;
+	cocos2d::MenuItem*      _decRot;
+	unsigned int   _curState;
+	cocos2d::MoveTo* _moveAction;
+	bool _bZoomOut;
+	bool _bZoomIn;
+	bool _bRotateLeft;
+	bool _bRotateRight;
+	cocos2d::Label* _RotateRightlabel;
+	cocos2d::Label* _RotateLeftlabel;
+	cocos2d::Label* _ZoomInlabel;
+	cocos2d::Label* _ZoomOutlabel;
+
 
      //implement the "static create()" method manually
 };
